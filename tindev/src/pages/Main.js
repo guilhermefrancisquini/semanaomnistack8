da8d729c1807cf8a96e98be1accb84828aab55f8
Guilherme <guilherme.francisquini@hotmail.com>
2019-08-11 21:24:37 -0300

    Implementa funcionalidade de match no mobile

--- tindev/src/pages/Main.js
+++ tindev/src/pages/Main.js
@@ -1,4 +1,5 @@
 import React, { useEffect, useState } from 'react';
+import io from 'socket.io-client';
 import AsyncStorage from '@react-native-community/async-storage';
 import { View, SafeAreaView, Image, StyleSheet, Text, TouchableOpacity } from 'react-native';
 
@@ -7,10 +8,12 @@ import api from '../services/api';
 import logo from '../assets/logo.png';
 import like from '../assets/like.png';
 import dislike from '../assets/dislike.png';
+import itsamatch from '../assets/itsamatch.png';
 
 export default function Main({ navigation }) {
     const id = navigation.getParam('user');
     const [users, setUsers] = useState([]);
+    const [matchDev, setMatchDev] = useState(null);
     
     useEffect( () => {
         async function loadUsers() {
@@ -23,6 +26,17 @@ export default function Main({ navigation }) {
         }
 
         loadUsers();
+    }, [id]);
+
+    useEffect(() => {
+        const socket = io('http://localhost:3333', {
+            query: { user: id },
+        });
+
+        socket.on('match', dev => {
+            setMatchDev(dev);
+        });
+
     }, [id]);
 
     async function handleLike() {
@@ -81,6 +95,20 @@ export default function Main({ navigation }) {
                     </TouchableOpacity>
                 </View> 
             ) }
+
+            { matchDev && (
+                <View style={styles.matchContainer}>
+                    <Image style={styles.matchImage} source={itsamatch} />
+                    <Image style={styles.matchAvatar} source={ { uri: matchDev.avatar }} />
+
+                    <Text style={styles.matchName}>{matchDev.name}</Text>
+                    <Text style={styles.matchBio}>{matchDev.bio}</Text>
+
+                    <TouchableOpacity onPress={() => setMatchDev(null)}>
+                        <Text style={styles.matchClose}>FECHAR</Text>
+                    </TouchableOpacity>
+                </View>
+            )} 
         </SafeAreaView>
     );
 }
@@ -159,5 +187,42 @@ const styles = StyleSheet.create({
         color: '#999',
         fontSize: 24,
         fontWeight: 'bold',
+    },
+    matchContainer: {
+        ... StyleSheet.absoluteFillObject,
+        backgroundColor: 'rgba(0,0,0,0.8)',
+        justifyContent: 'center',
+        alignItems: 'center',
+    },
+    matchAvatar: {
+        width: 160,
+        height: 160,
+        borderRadius: 80,
+        borderWidth: 5,
+        borderColor: '#FFF',
+        marginVertical: 30,
+    },
+    matchName: {
+        fontSize: 26,
+        fontWeight: 'bold',
+        color: '#FFF',
+    },
+    matchBio: {
+        marginTop: 10,
+        fontSize:26,
+        color: 'rgba(255, 255, 255, 0.8)',
+        textAlign: 'center',
+        paddingHorizontal: 30,
+    },
+    matchClose: {
+        fontSize: 16,
+        color: 'rgba(255, 255, 255, 0.8)',
+        textAlign: 'center',
+        marginTop: 30,
+        fontWeight: 'bold',
+    },
+    matchImage: {
+        height: 60,
+        resizeMode: 'contain',
     }
-})
+});
